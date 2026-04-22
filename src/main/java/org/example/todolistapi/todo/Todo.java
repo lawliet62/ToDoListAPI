a181ec2bd07781dfcdcc42363b464b571cffe8d3
lawliet62 <dlwldn62@gmail.com>
2026-04-22 21:09:26 +0900

    JPA 어노테이션을 사용하여 Todo 엔티티 추가

---
+++ src/main/java/org/example/todolistapi/todo/Todo.java
@@ -0,0 +1,30 @@
+package org.example.todolistapi.todo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String title;
+
+    @Setter
+    private String description;
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
