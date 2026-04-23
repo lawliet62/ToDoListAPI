9abdccabad5951029ef83796f4785c4839f9bf1f
lawliet62 <dlwldn62@gmail.com>
2026-04-23 20:40:15 +0900

    `User` 엔티티 추가 및 JPA 매핑 구성

---
+++ src/main/java/org/example/todolistapi/user/entity/User.java
@@ -0,0 +1,33 @@
+package org.example.todolistapi.user.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
