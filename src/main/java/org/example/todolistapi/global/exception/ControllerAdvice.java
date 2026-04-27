0e71673a6945f5fb471e36086ec21e32af1daca6
lawliet62 <dlwldn62@gmail.com>
2026-04-27 23:07:21 +0900

    전역 예외 처리에 새로운 예외 핸들러 추가 (DuplicateFormatFlagsException, InvalidCredentialsException, TodoNotFoundException)

--- src/main/java/org/example/todolistapi/global/exception/ControllerAdvice.java
+++ src/main/java/org/example/todolistapi/global/exception/ControllerAdvice.java
@@ -5,11 +5,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.DuplicateFormatFlagsException;
+
 @RestControllerAdvice
 public class ControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(DuplicateFormatFlagsException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateEmail(DuplicateFormatFlagsException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentials(InvalidCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(TodoNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleTodoNotFound(TodoNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(e.getMessage()));
     }
