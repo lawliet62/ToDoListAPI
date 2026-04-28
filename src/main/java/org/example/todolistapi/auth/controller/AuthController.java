125e00631076c597f4cc48cca0c69b233546751e
lawliet62 <dlwldn62@gmail.com>
2026-04-28 22:59:28 +0900

    `AuthController 추가하여 회원가입 및 로그인 API 구현`

---
+++ src/main/java/org/example/todolistapi/auth/controller/AuthController.java
@@ -0,0 +1,44 @@
+package org.example.todolistapi.auth.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.todolistapi.auth.dto.AuthResponse;
+import org.example.todolistapi.auth.dto.LoginRequest;
+import org.example.todolistapi.auth.dto.RegistrationRequest;
+import org.example.todolistapi.auth.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegistrationRequest request) {
+        AuthResponse response = authService.registerUser(
+                request.name(),
+                request.email(),
+                request.password()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(
+                request.email(),
+                request.password()
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+}
