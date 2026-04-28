c538ef71b4468f6c43fa41a6224b9fb64a770038
lawliet62 <dlwldn62@gmail.com>
2026-04-28 22:06:51 +0900

    `ControllerAdvice에서 불필요한 예외 핸들러 메서드 제거하여 코드 정리`

--- src/main/java/org/example/todolistapi/global/exception/ControllerAdvice.java
+++ src/main/java/org/example/todolistapi/global/exception/ControllerAdvice.java
@@ -5,22 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.DuplicateFormatFlagsException;
-
 @RestControllerAdvice
 public class ControllerAdvice {
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(DuplicateFormatFlagsException.class)
-    public ResponseEntity<ErrorResponse> handleDuplicateEmail(DuplicateFormatFlagsException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new ErrorResponse(e.getMessage()));
-    }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleInvalidCredentials(InvalidCredentialsException e) {
@@ -28,9 +14,7 @@ public class ControllerAdvice {
                 .body(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(TodoNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleTodoNotFound(TodoNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(e.getMessage()));
-    }
+
+
+
 }
