a1560316661c372e7e3f1da7896ded16593ca433
lawliet62 <dlwldn62@gmail.com>
2026-04-28 23:01:11 +0900

    `SecurityConfig에서 로그인 및 회원가입 경로를 /auth로 수정`

--- src/main/java/org/example/todolistapi/global/config/SecurityConfig.java
+++ src/main/java/org/example/todolistapi/global/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "register").permitAll()
+                        .requestMatchers("/auth/register", "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
