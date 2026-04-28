c37a9410f8c7c9e542b55c93ae7ff33a29827471
lawliet62 <dlwldn62@gmail.com>
2026-04-28 22:14:41 +0900

    `TodoService에 User 연관관계 추가 및 작성자 기반 Todo 생성 로직 구현`

--- src/main/java/org/example/todolistapi/todo/service/TodoService.java
+++ src/main/java/org/example/todolistapi/todo/service/TodoService.java
@@ -5,27 +5,36 @@ import org.example.todolistapi.todo.dto.TodoListResponse;
 import org.example.todolistapi.todo.dto.TodoResponse;
 import org.example.todolistapi.todo.entity.Todo;
 import org.example.todolistapi.todo.repository.TodoRepository;
+import org.example.todolistapi.user.entity.User;
+import org.example.todolistapi.user.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-class TodoService {
+public class TodoService {
 
-    private final TodoRepository repository;
+    private final TodoRepository todoRepository;
+    private final UserRepository userRepository;
 
-    public void createTodo(String title, String description) {
-        Todo todo = new Todo(title, description);
-        repository.save(todo);
+    @Transactional
+    public void createTodo(String title, String description, String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Todo todo = new Todo(title, description, user);
+        todoRepository.save(todo);
     }
 
+    @Transactional(readOnly = true)
     public TodoListResponse getTodos(int page, int limit) {
         Pageable pageable = PageRequest.of(page - 1, limit);
-        Page<Todo> todoPage = repository.findAll(pageable);
+        Page<Todo> todoPage = todoRepository.findAll(pageable);
 
         List<TodoResponse> data = todoPage.getContent().stream()
                 .map(todo -> new TodoResponse(
