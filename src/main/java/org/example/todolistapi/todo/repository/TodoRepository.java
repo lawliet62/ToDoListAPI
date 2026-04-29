f9d8d0c7503de5a77cf5d1d56789349eda14f3f4
lawliet62 <dlwldn62@gmail.com>
2026-04-29 18:03:51 +0900

    `TodoRepository에 사용자별 할 일 조회 메서드 추가`

--- src/main/java/org/example/todolistapi/todo/repository/TodoRepository.java
+++ src/main/java/org/example/todolistapi/todo/repository/TodoRepository.java
@@ -1,7 +1,11 @@
 package org.example.todolistapi.todo.repository;
 
 import org.example.todolistapi.todo.entity.Todo;
+import org.example.todolistapi.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    Page<Todo> findByUser(User user, Pageable pageable);
 }
