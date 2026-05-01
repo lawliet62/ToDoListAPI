53a5964bd1da0959386b4f604818b46893de1c0a
lawliet62 <dlwldn62@gmail.com>
2026-05-01 16:53:48 +0900

    `TodoController에 CRUD 메서드 추가 및 페이징 처리 구현`

--- src/main/java/org/example/todolistapi/todo/controller/TodoController.java
+++ src/main/java/org/example/todolistapi/todo/controller/TodoController.java
@@ -1,15 +1,16 @@
 package org.example.todolistapi.todo.controller;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.example.todolistapi.todo.dto.TodoCreateRequest;
+import org.example.todolistapi.todo.dto.TodoListResponse;
+import org.example.todolistapi.todo.dto.TodoResponse;
+import org.example.todolistapi.todo.dto.TodoUpdateRequest;
 import org.example.todolistapi.todo.service.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,14 +20,53 @@ public class TodoController {
     private final TodoService todoService;
 
     @PostMapping
-    public ResponseEntity<Void> createTodo(@RequestBody TodoCreateRequest request,
-                                           Authentication authentication) {
+    public ResponseEntity<Void> createTodo(Authentication authentication,
+                                           @RequestBody TodoCreateRequest request) {
         todoService.createTodo(
                 (Long) authentication.getPrincipal(),
                 request.title(),
                 request.description()
         );
+
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<TodoListResponse> getTodos(Authentication authentication,
+                                                     @RequestParam @Min(1) int page,
+                                                     @RequestParam @Min(1) int limit) {
+        TodoListResponse response = todoService.getTodos(
+                (Long) authentication.getPrincipal(),
+                page,
+                limit
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{todoId}")
+    public ResponseEntity<TodoResponse> updateTodo(Authentication authentication,
+                                                   @PathVariable Long todoId,
+                                                   @RequestBody TodoUpdateRequest request) {
+        TodoResponse response = todoService.updateTodo(
+                (Long) authentication.getPrincipal(),
+                todoId,
+                request.title(),
+                request.description()
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<Void> deleteTodo(Authentication authentication,
+                                           @PathVariable Long todoId) {
+        todoService.deleteTodo(
+                (Long) authentication.getPrincipal(),
+                todoId
+        );
+
+        return ResponseEntity.noContent().build();
     }
 
 }
