e9c76aa1b1dee50367655d704d43a0f679436e7c
lawliet62 <dlwldn62@gmail.com>
2026-05-06 20:08:30 +0900

    `@ConfigurationPropertiesScan 추가하여 구성 속성 클래스 자동 스캔 지원`

--- src/main/java/org/example/todolistapi/ToDoListApiApplication.java
+++ src/main/java/org/example/todolistapi/ToDoListApiApplication.java
@@ -2,8 +2,10 @@ package org.example.todolistapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ToDoListApiApplication {
 
     public static void main(String[] args) {
